afd646c9887e6c81454d6e27db1e65eef902a12a
YamilH <yamilhamui@gmail.com>
2023-08-15 10:12:50 -0300

    env

--- src/app/environments/environments.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: "AIzaSyB1KWENE8yM-lk6I60qiyvrhNFnx1jJiEM",
-  authDomain : "mis-clientes-5059c.firebaseapp.com"
-
-}
--- src/app/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiKey: "AIzaSyB1KWENE8yM-lk6I60qiyvrhNFnx1jJiEM",
-};
